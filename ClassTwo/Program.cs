141966240de36ce128481d5039114d31b0c6fab0
Carlos Jesus Caro <carlosjesuscaro@icloud.com>
2023-03-23 02:05:23 -0400

    ClassTwo - Basic class example

--- ClassTwo/Program.cs
+++ ClassTwo/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ClassTwo;
+
 Console.WriteLine("Hello, World!");
+Square sqr = new Square();
+Console.WriteLine(sqr.ComputeArea());
+Console.WriteLine(sqr.ComputePerimeter());
--- ClassTwo/Square.cs
+++ ClassTwo/Square.cs
@@ -2,5 +2,55 @@ namespace ClassTwo;
 
 public class Square
 {
+    // Attributes
+    private int x;
+    private int y;
+    private int size;
+
+    // Another example on setting up getter and setter
+    // public int example { get; set; }
     
+    // Getter
+    public int getSize()
+    {
+        return this.size;
+    }
+
+    // Setter
+    public void setSize(int size)
+    {
+        if (size > 0)
+        {
+            this.size = size;
+        }
+    }
+    
+    // Constructor
+    public Square()
+    {
+        this.x = 0;
+        this.y = 0;
+        this.size = 1;
+    }
+    
+    // Another example of constructor
+    /*
+    public Square(int x, int y, int size)
+    {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+    */
+    
+    // Methods
+    public int ComputePerimeter()
+    {
+        return this.size * 4;
+    }
+    public int ComputeArea()
+    {
+        return this.size * this.size;
+    }
+
 }
