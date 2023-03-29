79a24aba93b65a20c4722253e9c7fcbd41b1f7a2
Carlos Jesus Caro <carlosjesuscaro@icloud.com>
2023-03-28 22:51:18 -0400

    LinkedList example

--- DataStructures/Person.cs
+++ DataStructures/Person.cs
@@ -2,5 +2,24 @@ namespace DataStructures;
 
 public class Person
 {
-    
+    public string Firstname;
+    public string Lastname;
+    public string Country;
+    public string Fullname
+    {
+        get { return $"{this.Firstname} {this.Lastname}"; }
+    }
+
+    public Person(string firstname, string lastname, string country)
+    {
+        this.Firstname = firstname;
+        this.Lastname = lastname;
+        this.Country = country;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Fullname} - {this.Country}";
+    }
+
 }
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,3 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using DataStructures;
+
+LinkedList<string> strLList = new LinkedList<string>();
+LinkedList<Person> persList = new LinkedList<Person>();
+
+persList.AddLast(new Person("Clement", "Ziane", "France"));
+persList.AddFirst(new Person("Carlos", "Jesus", "Peru"));
+persList.AddAfter(persList.First, new Person("Alyson", "Caro", "Sweden"));
+persList.AddBefore(persList.First, new Person("Natalia", "Jesus Caro", "Canada"));
+
+LinkedListNode<Person>? anchor = persList.First;
+
+while (anchor != null)
+{
+    if (anchor.Value.Firstname == "Natalia")
+    {
+        break;
+    }
+    else
+    {
+        anchor = anchor.Next;
+    }
+}
+
+persList.AddAfter(anchor, new Person("Lucas", "Jesus Caro", "Canada"));
+
+foreach (Person person in persList)
+{
+    Console.WriteLine(person);
+}
+
+
