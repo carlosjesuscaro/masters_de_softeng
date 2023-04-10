c3565ba6bc8e9d58a8bfbaa04ee8016500a1292d
Carlos Jesus Caro <carlosjesuscaro@icloud.com>
2023-04-10 15:23:53 -0400

    Horizontal relations: aggregation

---
+++ Class Three/Car.cs
@@ -0,0 +1,9 @@
+namespace Class_Three;
+
+internal class Car
+{
+    public Wheel unmount(int wh)
+    {
+        
+    }
+}
---
+++ Class Three/Wheel.cs
@@ -0,0 +1,6 @@
+namespace Class_Three;
+
+internal class Wheel
+{
+    public Car attachedTo { get; set; }
+}
