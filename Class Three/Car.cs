0d37c63c9558e3e2970e02d870fe9ae705eaf4c1
Carlos Jesus Caro <carlosjesuscaro@icloud.com>
2023-04-10 15:26:32 -0400

    Horizontal relations: aggregation

--- Class Three/Car.cs
+++ Class Three/Car.cs
@@ -2,8 +2,27 @@ namespace Class_Three;
 
 internal class Car
 {
+    public Wheel[] wheels { get; } 
+    
+    public Car (int wheels)
+    {
+        this.wheels = new Wheel[wheels];
+        for (int i = 0; i < wheels; i++)
+        {
+            this.wheels[i] = new Wheel();
+            this.wheels[i].attach(this);
+        }   
+    }
+
     public Wheel unmount(int wh)
     {
-        
+        if (this.wheels[wh] != null)
+        {
+            this.wheels[wh].unmount();
+            Wheel result = this.wheels[wh];
+            this.wheels[wh] = null;
+            return result;
+        }
+        return null;
     }
 }
--- Class Three/Wheel.cs
+++ Class Three/Wheel.cs
@@ -1,6 +1,18 @@
+using System.Diagnostics;
+
 namespace Class_Three;
 
 internal class Wheel
 {
     public Car attachedTo { get; set; }
+
+    public void attach(Car attachedTo)
+    {
+        this.attachedTo = attachedTo;
+    }
+
+    public void unmount()
+    {
+        this.attachedTo = null;
+    }
 }
