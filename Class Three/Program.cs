b890d5e614de6b3dc1be5a909f77b194d1aa7a9a
Carlos Jesus Caro <carlosjesuscaro@icloud.com>
2023-04-10 13:23:20 -0400

    Adding comments

--- Class Three/Program.cs
+++ Class Three/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+// Horizontal relations
+// Association: teacher and student
+// Composition: head and person
+// Aggregation: car and wheel
