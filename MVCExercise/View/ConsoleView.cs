e772ba2999dd835ede682a58a7e62400625cbd6b
Carlos Jesus Caro <carlos.jesus.caro@gmail.com>
2023-04-29 20:05:41 -0400

    Partial implementation

---
+++ MVCExercise/View/ConsoleView.cs
@@ -0,0 +1,19 @@
+namespace MVCExercise.View;
+
+internal class ConsoleView
+{
+    public void printChoice()
+    {
+        Console.WriteLine("What mwould you like to do?");
+        Console.WriteLine("1 - Show details");
+        Console.WriteLine("2 - Edit");
+        Console.WriteLine("3 - Add a person");
+        Console.WriteLine("4 - Remove a person");
+        Console.WriteLine("5 - Exit");
+    }
+
+    public void printAddChoice()
+    {
+        
+    }
+}
